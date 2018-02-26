8f44ca538f6781597e48025647ec2bcaf9eda684
Matti Bijnens <matti.bijnens@student.howest.be>
2018-02-26 10:05:47 +0100

    Add project files.

---
+++ OefeningenLes3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OefeningenLes3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
